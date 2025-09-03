33a3eecd3df7e0e881363c5a4059b02be8a228e0
gsmit512 <gsmit512@students.bju.edu>
2025-09-03 15:30:54 -0400

    Changes

--- .github/Hello.java
+++ .github/Hello.java
@@ -2,9 +2,10 @@ import java.util.*;
 
 class Hello {
     public static void main(String[] args) {
-        Scanner scan = new Scanner();
+        Scanner scan = new Scanner(System.in);   // Scanner needs an argument
         int num1 = Integer.parseInt(scan.nextLine());
         int num2 = Integer.parseInt(scan.nextLine());
-        System.out.println("The sum is: " + num1 + num2);
+        System.out.println("The sum is: " + (num1 + num2));
+        scan.close();   //Scanner needs to be closed
     }
 }   
