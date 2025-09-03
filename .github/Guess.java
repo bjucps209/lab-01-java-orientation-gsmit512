5231dea0395c63db40e743716bff61289e384eff
gsmit512 <gsmit512@students.bju.edu>
2025-09-03 16:47:11 -0400

    Feedback

--- .github/Guess.java
+++ .github/Guess.java
@@ -2,15 +2,24 @@ import java.util.*;
 
 public class Guess {    
     public static void main(String[] args) {
-        Random random = new Random();    // Generate secret number
+        Random random = new Random();
         Scanner scan = new Scanner(System.in);
-        int secretNum = random.nextInt(10) + 1;  // Options: 1-10
+        int secretNum = random.nextInt(10) + 1;  // Generate secret number (options: 1-10)
         int numGuesses = 0;
+        int guess = 0;
 
-        System.out.print("Enter your guess: ");
-        int guess = Integer.parseInt(scan.nextLine());
-        // while (guess != secretNum){
-
-        // }
+        // Give feedback on guess
+        while (guess != secretNum){
+            System.out.print("Enter your guess: ");
+            guess = Integer.parseInt(scan.nextLine());
+            if (guess < secretNum){
+                System.out.println("Your guess is too low.");
+            } else if (guess > secretNum){
+                System.out.println("Your guess is too high.");
+            } else {
+                System.out.println("You got it!!");
+            }
+        }
+        scan.close();
     }
 }
