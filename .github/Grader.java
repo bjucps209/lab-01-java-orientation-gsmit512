1e8ae211ead34f598d107057a802701ce457dec8
gsmit512 <gsmit512@students.bju.edu>
2025-09-04 12:37:12 -0400

    Part C: Loop for number of students

--- .github/Grader.java
+++ .github/Grader.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class Grader {
     public static void main(String[] args) {
-        
+        //  Loop for the number of students
+        for (int i = 0; i < args.length; i++) {
+            
+        }
     }
 }
