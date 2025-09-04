f0df9fbd828ac059242c6e0b918e21f9ab8ab986
gsmit512 <gsmit512@students.bju.edu>
2025-09-04 12:44:47 -0400

    Part C: Properly grabs the number of students and loops that many times

--- .github/Grader.java
+++ .github/Grader.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class Grader {
     public static void main(String[] args) {
-        //  Loop for the number of students
-        for (int i = 0; i < args.length; i++) {
-            
+        int nStudents = Integer.parseInt(args[0]);  // Grab the number of students
+        for (int i = 0; i < nStudents; i++) {
+            System.out.println("Hi there!");
         }
     }
 }
