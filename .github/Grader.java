82f4b49e5c91a01a75979857bbe7e7f77e2e405f
gsmit512 <gsmit512@students.bju.edu>
2025-09-04 13:00:43 -0400

    Part C: Updated

--- .github/Grader.java
+++ .github/Grader.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 public class Grader {
     public static void main(String[] args) {
-        int nStudents = Integer.parseInt(args[0]);  // Grab the number of students
+        Scanner scan = new Scanner(System.in);  //  Use scanner for pipeline usage
+        int nStudents = scan.nextInt();  // Grab the number of students
         for (int i = 0; i < nStudents; i++) {
-            System.out.println("Hi there!");
+            System.out.println(scan.nextInt());
         }
+        scan.close();
     }
 }
